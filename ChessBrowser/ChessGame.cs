1b541ed2870ed771e107a33e1c6f44016ad94b09
Charlotte Saethre <charlotte.saethre@gmail.com>
2024-03-11 19:14:09 -0600

    adds library classes

---
+++ ChessBrowser/ChessGame.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessBrowser
+{
+    internal class ChessGame
+    {
+        public string Event {  get; set; }
+        public string Site {  get; set; }
+        public string Round {  get; set; }
+        public string WhitePlayer { get; set; }
+        public string BlackPlayer { get; set; }
+        public int WhiteElo { get; set; }
+        public int BlackElo { get; set; }
+        public string Result { get; set; }
+        public DateTime EventDate { get; set; }
+
+        public string Moves { get; set; }
+
+        public ChessGame(string Event, string Site, string Round, 
+            string WhitePlayer, string BlackPlayer, int WhiteElo, 
+            int BlackElo, string Result, DateTime EventDate, string Moves)
+        {
+            this.Event = Event;
+            this.Site = Site;
+            this.Round = Round;
+            this.WhitePlayer = WhitePlayer;
+            this.BlackPlayer = BlackPlayer;
+            this.WhiteElo = WhiteElo;
+            this.BlackElo = BlackElo;
+            this.Result = Result;
+            this.EventDate = EventDate;
+            this.Moves = Moves;
+        }
+
+    }
+}
---
+++ ChessBrowser/PgnReader.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ChessBrowser
+{
+    internal static class PgnReader
+    {
+        static List<ChessGame> readPgnFile(string path)
+        {
+            List<ChessGame> games = new List<ChessGame>();
+            string[] lines = File.ReadAllLines(path);
+
+            string Event, Site, Round, WhitePlayer, BlackPlayer, Result;
+            int WhiteElo, BlackElo;
+            DateTime EventDate;
+
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(" ");
+                switch (parts[0])
+                {
+                    case "[Event":
+                        break;
+                    case "[Site":
+                        break;
+                    case "[Date":
+                        break;
+                    case "[Round":
+                        break;
+                    case "[White":
+                        break;
+                    case "[Black":
+                        break;
+                    case "[Result":
+                        break;
+                    case "[WhiteElo":
+                        break;
+                    case "BlackElo":
+                        break;
+                    case "[EventDate":
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return games;
+        }
+    }
+}
